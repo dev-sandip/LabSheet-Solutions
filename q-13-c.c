a4ce7e942312711e57880107f1ca038b46487124
git-sandip <sandipsapkota9844@gmail.com>
2023-07-03 21:19:23 +0545

    another one

---
+++ q-13-c.c
@@ -0,0 +1,21 @@
+//    print the following pattern
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+
+
+
+#include <stdio.h>
+    
+int main()
+{
+    int i,j;
+
+for (i=1; i<=4; i++){
+    for (j=1;)
+}
+
+    return 0;
+}
