8150e3d66d99c4ad63253b69e330ab0c6ea88091
git-sandip <sandipsapkota9844@gmail.com>
2023-07-18 22:15:28 +0545

    static variable added

--- LabSheet-6/q8.c
+++ LabSheet-6/q8.c
@@ -8,6 +8,15 @@
         b=30;
       printf("From Function-2 a = %d and b = %d\n",a,b);
     }
+void incrementStatic() {
+    // Declare a static variable
+    static int count = 0;
+    
+    // Increment the static variable and print its value
+    count++;
+    printf("Static variable count: %d\n", count);
+}
+
 int main()
 {
     printf("Form main function a =%d and b = %d\n",a,b);
@@ -15,5 +24,13 @@ int main()
     func_2();
     a=20;
     func_1();
+ // Call the incrementStatic function multiple times
+     for (int i = 0; i < 5; i++) {
+    incrementStatic();
+    }
     return 0;
 }
+#include <stdio.h>
+
+
+
