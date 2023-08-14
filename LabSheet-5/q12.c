59c0ee070bbc7a31dcd18d76e73540d3e751eace
git-sandip <sandipsapkota9844@gmail.com>
2023-08-14 20:00:16 +0545

    code refined

--- LabSheet-5/q12.c
+++ LabSheet-5/q12.c
@@ -1,24 +1,21 @@
 //WAP to find HCF and LCM of two numbers.
 #include <stdio.h>
 
-int main() {
-    int num1, num2, hcf, lcm, smaller;
-
-    printf("Enter two numbers: ");
-    scanf("%d %d", &num1, &num2);
-
-    smaller = (num1 < num2) ? num1 : num2;
-
-    for (hcf = smaller; hcf >= 1; hcf--) {
-        if (num1 % hcf == 0 && num2 % hcf == 0) {
-            break;
-        }
-    }
-
-    lcm = (num1 * num2) / hcf;
-
-    printf("HCF: %d\n", hcf);
-    printf("LCM: %d\n", lcm);
-
-    return 0;
+#include <stdio.h>
+    
+int main()
+{
+int a,b,hcf,lcm ,i;
+printf("Enter the value of a\n");
+scanf("%d", &a);
+printf("Enter the value of b\n");
+scanf("%d", &b);
+for (i=1 ; i<=a ; i++){
+if ( a%i==0 && b%i==0){
+    hcf=i;
+}
+}
+lcm = (a*b)/hcf;
+printf("%d is HCF\n",hcf);
+printf("%d is LCM\n",lcm); return 0;
 }
